81e744e9fe3cbae204f44faed8ce2f2705ab8de5
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2022-11-04 17:36:10 +0800

    learning kivy

--- kivy_experiment/main.py
+++ kivy_experiment/main.py
@@ -1,13 +1,37 @@
 from kivy.app import App
 from kivy.metrics import dp
+from kivy.properties import StringProperty
+from kivy.properties import BooleanProperty
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
-from kivy.uix.scrollview import ScrollView
+# from kivy.uix.scrollview import ScrollView
+# from kivy.uix.pagelayout import PageLayout
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 
+
+class WidgetsExample(GridLayout):
+    count = 1
+    count_enabled = BooleanProperty(False)
+    my_string = StringProperty('1')
+    def on_button_click(self):   
+        if self.count_enabled:
+            self.count+=1
+            self.my_string = str(self.count)
+        
+    def on_toggle_button_state(self, widget):
+        print("toggle state: " + widget.state)
+        if widget.state == "normal":
+            widget.text = "OFF"
+            self.count_enabled = False
+        else:
+            widget.text = "ON"
+            self.count_enabled = True
+
+        
+        
 
 # class ScrollViewExample(ScrollView):
 #     ...
@@ -22,8 +46,8 @@ class StackLayoutExample(StackLayout):
             b = Button(text=str(i+1),size_hint=(None,None), size=(dp(size), dp(size)))
             self.add_widget(b)
 
-class GridLayouttExample(GridLayout):
-    ...
+# class GridLayouttExample(GridLayout):
+#     ...
 
 class AnchorLayoutExample(AnchorLayout):
     ...
