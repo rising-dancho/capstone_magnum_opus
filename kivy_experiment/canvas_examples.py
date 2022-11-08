e83a946f537fff4d155b1bb449158c80b5e61a8c
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2022-11-08 09:58:15 +0800

    learning kivy

---
+++ kivy_experiment/canvas_examples.py
@@ -0,0 +1,11 @@
+from kivy.lang.builder import Builder
+from kivy.uix.widget import Widget
+
+Builder.load_file("canvas_examples.kv")
+
+
+class CanvasExample1(Widget):
+    ...
+
+class CanvasExample2(Widget):
+    ...
--- kivy_experiment/main.py
+++ kivy_experiment/main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.properties import ObjectProperty
 from navigation_screen_manager import NavigationScreenManager
+from canvas_examples import CanvasExample1
+from canvas_examples import CanvasExample2
 # #:import CardTransition kivy.uix.screenmanager.CardTransition
 # https://kivy.org/doc/stable/api-kivy.uix.screenmanager.html?highlight=screenmanager#module-kivy.uix.screenmanager
 
@@ -13,6 +15,7 @@ class TheLabApp(App):
      
     def build(self):
         self.manager = MyScreenManager()
-        return self.manager
+        #return self.manager
+        return CanvasExample2()
 
 TheLabApp().run()
