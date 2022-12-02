e9ce474a963b015200cdb93002feb032d326cf3f
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2022-12-02 18:26:28 +0800

    learning kivy

--- kivy_experiment/firebase/KivyApp.py
+++ kivy_experiment/firebase/KivyApp.py
@@ -11,16 +11,60 @@ class KivyApp(App):
 
     def build(self):
         box_layout = BoxLayout()
-        button = Button(text="Create patch")
-        button.bind(on_press=self.create_patch)
-        box_layout.add_widget(button)
+        box_layout.orientation = "vertical"
+
+        # button_patch = Button(text="create patch")
+        # button_patch.bind(on_press=self.create_patch)
+
+        button_get = Button(text="get data")
+        button_get.bind(on_press=self.create_get)
+
+        button_post = Button(text="get post")
+        button_post.bind(on_press=self.create_post)
+
+        button_put = Button(text="get put")
+        button_put.bind(on_press=self.create_put)
+
+        button_delete = Button(text="get delete")
+        button_delete.bind(on_press=self.create_delete)
+        
+        # box_layout.add_widget(button_patch)
+        box_layout.add_widget(button_get)
+        box_layout.add_widget(button_post)
+        box_layout.add_widget(button_put)
+        box_layout.add_widget(button_delete)
+
         return box_layout
 
-    def create_patch(self, *kwargs):
-        print("BUTTON CLICKED")
-        json_data = '{"url": "google.com", "age": "15 years old"}'
-        response = requests.patch(url=self.firebase_url, json=json.loads(json_data))
+    # def create_patch(self, *kwargs):
+    #     print("BUTTON GET CLICKED")
+    #     json_data = '{"url": "amazon.com", "age": "17 years old"}'
+    #     response = requests.patch(url=self.firebase_url, json=json.loads(json_data))
+    #     print(response) 
+
+    def create_get(self, *kwargs):
+        print("BUTTON GET CLICKED")
+        response = requests.get(url=self.firebase_url)
+        print(response.json())
+
+    def create_post(self, *kwargs  ):
+        print("BUTTON POST CLICKED")
+        json_data = '{"Table1":{"url": "amazon.com", "age": "17 years old"}}'
+        response = requests.post(url=self.firebase_url, json=json.loads(json_data))
         print(response)
+
+    def create_put(self, *kwargs):
+        print("BUTTON PUT CLICKED")
+        json_data = '{"Table1":{"url": "google.com", "age": "25 years old"}}'
+        response = requests.put(url=self.firebase_url, json=json.loads(json_data))
+        print(response)
+
+    def create_delete(self, *kwargs):
+        print("BUTTON DELETE CLICKED")
+        delete_url ="https://kivycrudproject-fbca8-default-rtdb.firebaseio.com/"
+        response = requests.patch(url=delete_url+"Table1/url"+".json")
+        print(response)
+
 if __name__ == "__main__":
     KivyApp().run()
 
