bea22cdefb1226c03c56e1bbde3604390ab5e7e6
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2022-12-03 10:24:32 +0800

    learning kivy

--- kivy_experiment/firebase/KivyApp.py
+++ kivy_experiment/firebase/KivyApp.py
@@ -19,13 +19,13 @@ class KivyApp(App):
         button_get = Button(text="get data")
         button_get.bind(on_press=self.create_get)
 
-        button_post = Button(text="get post")
+        button_post = Button(text="post data")
         button_post.bind(on_press=self.create_post)
 
-        button_put = Button(text="get put")
+        button_put = Button(text="put data")
         button_put.bind(on_press=self.create_put)
 
-        button_delete = Button(text="get delete")
+        button_delete = Button(text="delete data")
         button_delete.bind(on_press=self.create_delete)
         
         # box_layout.add_widget(button_patch)
@@ -51,19 +51,20 @@ class KivyApp(App):
         print("BUTTON POST CLICKED")
         json_data = '{"Table1":{"url": "amazon.com", "age": "17 years old"}}'
         response = requests.post(url=self.firebase_url, json=json.loads(json_data))
-        print(response)
+        print(response.json())
 
     def create_put(self, *kwargs):
+        firebase_put_url = "https://kivycrudproject-fbca8-default-rtdb.firebaseio.com/Table2.json"
         print("BUTTON PUT CLICKED")
-        json_data = '{"Table1":{"url": "google.com", "age": "25 years old"}}'
-        response = requests.put(url=self.firebase_url, json=json.loads(json_data))
-        print(response)
+        json_data = '{"age":"18 years old"}'
+        response = requests.put(url=firebase_put_url, json=json.loads(json_data))
+        print(response.json())
 
     def create_delete(self, *kwargs):
         print("BUTTON DELETE CLICKED")
         delete_url ="https://kivycrudproject-fbca8-default-rtdb.firebaseio.com/"
-        response = requests.patch(url=delete_url+"Table1/url"+".json")
-        print(response)
+        response = requests.delete(url=delete_url+"Table2/url"+".json")
+        print(response.json())
 
 if __name__ == "__main__":
     KivyApp().run()
