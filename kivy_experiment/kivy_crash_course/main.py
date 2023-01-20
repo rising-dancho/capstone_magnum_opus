9d57ade518ed4d548e03122330edb725d1b23546
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2023-01-20 16:43:44 +0800

    learning kivy

--- kivy_experiment/kivy_crash_course/main.py
+++ kivy_experiment/kivy_crash_course/main.py
@@ -7,8 +7,22 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 
+import random
+
 class ScatterTextWidget(BoxLayout):
-    ...
+    def change_label_colour(self, *args):
+        # colour = [random.random() for i in range(3)] + [1]
+        colour1  = random.random()
+        colour2  = random.random()
+        colour3  = random.random()
+        # colour4  = random.random()
+        colour4  = 1
+        colour = [colour1, colour2, colour3, colour4]
+
+        print(colour)
+        label = self.ids.my_label
+        label.color = colour
+
 
 class TutorialApp(App):
     def build(self):
