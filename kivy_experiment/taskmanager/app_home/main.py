83db9ebd9927ff12de529b82c0af4c42da8c2f05
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2023-01-23 19:30:35 +0800

    learning kivy framework

---
+++ kivy_experiment/taskmanager/app_home/main.py
@@ -0,0 +1,13 @@
+from app import MainApp
+from kivy.garden.iconfonts import register
+
+from kivy.lang import Builder
+Builder.load_file('main.kv')
+
+from os.path import dirname, join
+
+
+if __name__ == "__main__":
+	register('MatIcons',join(dirname(__file__), 'app/assets/fonts/Material-Design-Iconic-Font.ttf'), join(dirname(__file__), 'app/assets/fonts/zmd.fontd'))
+	
+	MainApp().run()
---
+++ kivy_experiment/taskmanager/kivy_ubuntu.sh
@@ -0,0 +1,7 @@
+# Install necessary system packages
+
+# install the correct Cython version
+pip3 install Cython==0.29.10
+pip3 install kivy
+pip3 install kivy-garden
+pip3 install buildozer
