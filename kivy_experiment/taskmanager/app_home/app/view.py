3d45d48434d7d0b1d57c96fa7f231e634235409e
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2023-02-28 16:33:06 +0800

    doing gods work right now

--- kivy_experiment/taskmanager/app_home/app/view.py
+++ kivy_experiment/taskmanager/app_home/app/view.py
@@ -1,4 +1,25 @@
 from kivy.uix.boxlayout import BoxLayout
 
 class MainWindow(BoxLayout):
-    ...
+    def __init__(self, **kw):
+        super().__init__(**kw)
+
+    def auth_user(self, username,  password):
+        """Authenticates a user given credentials
+        from inputs
+        
+        Args:
+            username (kivy.uix.textinput.TextInput): 
+                - textinput containing the username
+            
+            password (kivy.uix.textinput.TextInput): 
+                - textinput containing the password
+        
+        Returns:
+            None
+        """
+
+        uname = username.text
+        upass = password.text
+
+        self.ids.scrn_mngr.current = 'scrn_main'
