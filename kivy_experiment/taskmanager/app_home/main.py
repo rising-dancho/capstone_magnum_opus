512840bb874df8fa5bec8e0b1d9ed56ba4d3af5e
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2023-02-28 12:23:20 +0800

    welcome back! lol

--- kivy_experiment/taskmanager/app_home/main.py
+++ kivy_experiment/taskmanager/app_home/main.py
@@ -10,4 +10,4 @@ from os.path import dirname, join
 if __name__ == "__main__":
 	register('MatIcons',join(dirname(__file__), 'app/assets/fonts/Material-Design-Iconic-Font.ttf'), join(dirname(__file__), 'app/assets/fonts/zmd.fontd'))
 	
-	MainApp().run()
+	MainApp().run() 
--- kivy_experiment/taskmanager/main.py
+++
@@ -1,8 +0,0 @@
-from app import MainApp
-from kivy.garden.iconfonts import register
-
-from os.path import dirname, join
-
-if __name__ == '__main__':
-	register('MatIcons', dirname(__file__), 'app/assets/fonts/Material-Design-Iconic-Font.ttf'), join(dirname(__file__), 'app/assets/fonts/zmd.fontd'))
-	MainApp().run()
