d24216a8f3a952116e9153b106ef5abddefac834
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2023-03-03 16:44:59 +0800

    what a pain in the ballz

--- kivy_experiment/taskmanager/app_home/app/view.py
+++ kivy_experiment/taskmanager/app_home/app/view.py
@@ -1,23 +1,61 @@
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.uix.modalview import ModalView
+
+from kivy.properties import StringProperty, NumericProperty
+from kivy.metrics import sp,dp
+from kivy.utils import rgba
+
+class NewTask(ModalView):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+
+
+class Task(ButtonBehavior, BoxLayout):
+    """
+        Description of Task():
+        class representing a single task added by the user.
+
+        Args:
+            self (python special parameter):
+            **kwargs (undefined):
+
+        Attributes
+        ----------
+        name : str
+            task name
+        time : str
+            time when the task is expected to commence.
+
+    """
+    
+    name = StringProperty('')
+    time = StringProperty('')
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        
 
 class MainWindow(BoxLayout):
     def __init__(self, **kw):
         super().__init__(**kw)
-
+    
+    def add_new(self):
+        nt = NewTask()
+        nt.open()
+    
     def auth_user(self, username,  password):
-        """Authenticates a user given credentials
+        """
+        Authenticates a user given credentials
         from inputs
-        
+
         Args:
+            self (python special parameter):
             username (kivy.uix.textinput.TextInput): 
                 - textinput containing the username
-            
-            password (kivy.uix.textinput.TextInput): 
+            password kivy.uix.textinput.TextInput): 
                 - textinput containing the password
-        
-        Returns:
-            None
         """
+
 
         uname = username.text
         upass = password.text
