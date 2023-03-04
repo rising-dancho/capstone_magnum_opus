102037d5b76f8235753c999df9182dc508fea09d
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2023-03-05 03:17:55 +0800

    haha lol wlang may pake

--- kivy_experiment/taskmanager/app_home/app/view.py
+++ kivy_experiment/taskmanager/app_home/app/view.py
@@ -1,6 +1,8 @@
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.modalview import ModalView
+from kivy.garden.circulardatetimepicker import CircularTimePicker
+from kivy.uix.button import Button
 
 from kivy.properties import StringProperty, NumericProperty
 from kivy.metrics import sp,dp
@@ -9,6 +11,28 @@ from kivy.utils import rgba
 class NewTask(ModalView):
     def __init__(self, **kw):
         super().__init__(**kw)
+
+    def get_time(self):
+        mv = ModalView(size_hint=[.8,.8])
+        box = BoxLayout(orientation = 'vertical', size_hint=[.8,.8])
+        mv.add_widget(box)
+
+        cl = CircularTimePicker()
+        cl.bind(time= self.set_time)
+        
+        submit = Button(text="Okay", background_normal='', background_color=rgba('#282C34'), size_hint_y=.1)
+        submit.bind(on_release=lambda x: self.update_time(cl.time, mv))
+        box.add_widget(cl)
+        # box.add_widget(Button(background_disabled='', background_color=[1,1,1,0], disabled=True))
+        box.add_widget(submit)
+        mv.open()
+
+    def set_time(self, inst, value):
+        print(value)
+    
+    def update_time(self, time, mv):
+        mv.dismiss()
+        self.ids.task_time.text=str(time)
 
 
 class Task(ButtonBehavior, BoxLayout):
