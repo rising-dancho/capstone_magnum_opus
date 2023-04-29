12c401d9b5188ed5e5781451926ad5b002e6c5e8
adfinem_rising <43742265+rising-dancho@users.noreply.github.com>
2023-04-29 10:58:20 +0800

    tutorial hell

---
+++ kivy_experiment/practice/activity1/pong.py
@@ -0,0 +1,44 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.vector import Vector
+from kivy.clock import Clock
+
+class PongBall(Widget):
+
+    # velocity of the ball on x and y axis
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+
+    # referencelist property so we can use ball.velocity as
+    # a shorthand, just like e.g. w.pos for w.x and w.y
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    # ``move`` function will move the ball one step. This
+    #  will be called in equal intervals to animate the ball
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
+
+class PongGame(Widget):
+    ball = ObjectProperty(None)
+
+    def update(self, dt):
+        # call ball.move and other stuff
+        self.ball.move()
+
+        # bounce off top and bottom
+        if (self.ball.y < 0) or (self.ball.top > self.height):
+            self.ball.velocity_y *= -1
+        
+
+
+
+class PongApp(App):
+    def build(self):
+        game = PongGame()
+        Clock.schedule_interval(game.update, 1, 60)
+        return game()
+
+
+if __name__ == '__main__':
+    PongApp().run()
